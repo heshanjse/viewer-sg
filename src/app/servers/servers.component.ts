d5dea93712bf9afcc995544a89ba7558be42883f
Heshan Jayasinghe <heshan@Heshans-MacBook-Pro.local>
2021-05-29 13:40:21 +0800

    add twoway data bindings

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,14 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  allowNewServer = false;
+  allowNewServer = true;
+  serverCreationStatus = 'NO server was created!';
+  serverName = 'test Server'
   constructor() {
     setTimeout(()=>{
-      this.allowNewServer = true;
+      this.allowNewServer = false;
     },2000)
   }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was Created!';
+  }
+
+  onCreatewithServername(){
+    this.serverCreationStatus = 'server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event : any){
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
